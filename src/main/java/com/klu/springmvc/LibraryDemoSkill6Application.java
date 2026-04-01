1a1e87212bb179cd45fb289e133143b438f28e36
amritha <amrithavaishnavi904@gmail.com>
2026-04-01 14:06:45 +0530

    update files

--- src/main/java/com/klu/springmvc/LibraryDemoSkill6Application.java
+++ src/main/java/com/klu/springmvc/LibraryDemoSkill6Application.java
@@ -1,4 +1,9 @@
-package com.klu.springmvc;
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yourname/library-demo-skill6.git
+git push -u origin mainpackage com.klu.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
